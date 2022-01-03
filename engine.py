f90986f4bee2bf1b6afde6e7708844f9f227e8ee
chelovek12565 <bas180407@mail.tu>
2022-01-03 16:40:12 +0500

    Скрипт для создания карты, добавление анимации, ходьба

---
+++ engine.py
@@ -0,0 +1,96 @@
+from PIL import Image
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    image = pygame.image.load('data/animations/Idle/Idle_1.png')
+
+    def __init__(self, x, y, *args):
+        super(Player, self).__init__(*args)
+        self.first = True
+        self.anim_n = 60
+        self.align = 'right'
+        self.rect = self.image.get_rect()
+        self.rect.x = x * 32
+        self.rect.y = y * 32 + 14
+        self.status = 'idle'
+
+    def update(self, r=None):
+        if self.first:
+            self.first = False
+            x, y = r
+            self.rect.x = x + 32
+            self.rect.y = y + 47
+        self.anim_n += 1
+        if self.anim_n >= 601:
+            self.anim_n = 60
+        if self.status == 'idle':
+            if self.align == 'right':
+                self.image = pygame.image.load(f'data/animations/Idle/Idle_{self.anim_n // 60}.png')
+            else:
+                self.image = pygame.transform.flip(pygame.image.load(f'data/animations/Idle/Idle_{self.anim_n // 60}.png'), True, False)
+
+    def align_change(self, side):
+        if self.align != side:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.align = side
+        self.status = 'idle'
+        self.anim_n = 60
+
+
+class Camera:
+    def __init__(self, player_cord):
+        self.r = list(map(lambda i: 750 // 2 - i * 2, player_cord))
+
+    def movement(self, side=None):
+        print(self.r)
+        if not side:
+            return self.r
+        elif side == 'left':
+            x, y = self.r
+            self.r = [x + 1, y]
+            return self.r
+        elif side == 'right':
+            x, y = self.r
+            self.r = [x - 1, y]
+            return self.r
+        elif side == 'up':
+            x, y = self.r
+            self.r = [x, y - 1]
+            return self.r
+        elif side == 'down':
+            x, y = self.r
+            self.r = [x, y + 1]
+            return self.r
+
+
+class Level(pygame.sprite.Sprite):
+    def __init__(self, image, size, *args):
+        super(Level, self).__init__(*args)
+        self.image = pygame.image.frombuffer(image, size, 'RGB')
+        self.rect = self.image.get_rect()
+
+    def update(self, r):
+        x, y = r
+        self.rect.x = x
+        self.rect.y = y
+
+
+def load_level(filename):
+    file = open(f'data/levels/{filename}', 'rt').read().split('\n')
+    im = Image.new('RGB', (len(file[0]) * 32, len(file) * 32), (66, 40, 53))
+    wall = Image.open('data/wall.png')
+    wall_2 = Image.open('data/wall_2.png')
+    for i in range(len(file)):
+        for j in range(len(file[0])):
+            symbol = file[i][j]
+            if symbol == '#':
+                im.paste(wall_2, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
+            elif symbol == '@':
+                im.paste(wall, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
+                player_cord = (j, i)
+            else:
+                im.paste(wall, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
+    level = Level(im.tobytes(), im.size)
+    im.save('eagaegr.png')
+    return level, player_cord
---
+++ main.py
@@ -0,0 +1,43 @@
+from engine import *
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((750, 750))
+all_sprites = pygame.sprite.Group()
+screen.fill((66, 40, 53))
+level, player_cord = load_level('test_level.txt')
+all_sprites.add(level)
+walking = None
+player = Player(*player_cord, all_sprites)
+camera = Camera([player.rect.x, player.rect.y])
+r = camera.movement()
+all_sprites.update(r=r)
+clock = pygame.time.Clock()
+clock.tick(60)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                all_sprites.update(r)
+                player.align_change('right')
+                walking = 'right'
+            elif event.key == pygame.K_a:
+                walking = 'left'
+                player.align_change('left')
+            player.status = 'idle'
+            player.anim_n = 60
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_w, pygame.K_a, pygame.K_d, pygame.K_s]:
+                walking = None
+    if walking == 'left':
+        all_sprites.update(camera.movement('left'))
+    elif walking == 'right':
+        all_sprites.update(camera.movement('right'))
+    screen.fill((66, 40, 53))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    player.update()
+pygame.quit()
