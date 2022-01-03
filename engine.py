08201f58a9afcf0a2a366d3904be6bc913253fd6
chelovek12565 <bas180407@mail.tu>
2022-01-03 20:25:10 +0500

    Добавлен прыжок и его анимация

--- engine.py
+++ engine.py
@@ -8,19 +8,20 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, *args):
         super(Player, self).__init__(*args)
         self.first = True
+        self.jump_n = 0
+        self.jumping = False
         self.anim_n = 60
         self.align = 'right'
         self.rect = self.image.get_rect()
         self.rect.x = x * 32
-        self.rect.y = y * 32 + 14
+        self.rect.y = y * 32 + 20
         self.status = 'idle'
 
     def update(self, r=None):
         if self.first:
             self.first = False
-            x, y = r
-            self.rect.x = x + 32
-            self.rect.y = y + 52
+            self.rect.x = 750 // 2 - 32
+            self.rect.y = 750 // 2 - 52
         self.anim_n += 5
         if self.status == 'idle':
             if self.anim_n >= 600:
@@ -37,7 +38,13 @@ class Player(pygame.sprite.Sprite):
             if self.anim_n == 600:
                 self.anim_n = 60
             self.image = pygame.transform.flip(pygame.image.load(f'data/animations/Walk/Run_{self.anim_n // 60}.png'), True, False)
-
+        n = self.jump_n // 5
+        if not n:
+            n += 1
+        if self.jump_n and self.align == 'right':
+            self.image = pygame.image.load(f'data/animations/Jump/Jump_{n}.png')
+        elif self.jump_n and self.align == 'left':
+            self.image = pygame.transform.flip(pygame.image.load(f'data/animations/Jump/Jump_{n}.png'), True, False)
 
     def align_change(self, side):
         if self.align != side:
@@ -45,6 +52,20 @@ class Player(pygame.sprite.Sprite):
             self.align = side
         self.status = 'idle'
         self.anim_n = 60
+
+    def jump(self):
+        self.jump_n += 1
+        if self.jumping == 'up':
+            self.rect.y -= 6
+            if self.jump_n == 15:
+                self.jump_n = 0
+                self.jumping = 'down'
+        elif self.jumping == 'down':
+            self.rect.y += 6
+            if self.jump_n == 15:
+                self.jump_n = 0
+                self.jumping = False
+
 
 
 class Camera:
@@ -85,6 +106,10 @@ class Level(pygame.sprite.Sprite):
 
 
 def load_level(filename):
+    v_bord = []
+    h_bord = []
+    watching = False
+    bord = []
     file = open(f'data/levels/{filename}', 'rt').read().split('\n')
     im = Image.new('RGB', (len(file[0]) * 32, len(file) * 32), (66, 40, 53))
     wall = Image.open('data/wall.png')
@@ -94,11 +119,12 @@ def load_level(filename):
             symbol = file[i][j]
             if symbol == '#':
                 im.paste(wall_2, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
+                watching = True
             elif symbol == '@':
                 im.paste(wall, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
                 player_cord = (j, i)
             else:
                 im.paste(wall, (j * 32, i * 32, (j + 1) * 32, (i + 1) * 32))
+
     level = Level(im.tobytes(), im.size)
-    im.save('eagaegr.png')
     return level, player_cord
--- main.py
+++ main.py
@@ -7,7 +7,10 @@ all_sprites = pygame.sprite.Group()
 screen.fill((66, 40, 53))
 level, player_cord = load_level('test_level.txt')
 all_sprites.add(level)
-walking = None
+walking = {
+    'left': False,
+    'right': False
+}
 player = Player(*player_cord, all_sprites)
 camera = Camera([player.rect.x, player.rect.y])
 r = camera.movement()
@@ -23,22 +26,37 @@ while running:
             if event.key == pygame.K_d:
                 all_sprites.update(r)
                 player.align_change('right')
-                walking = 'right'
+                walking['right'] = True
                 player.status = 'right'
             elif event.key == pygame.K_a:
-                walking = 'left'
+                walking['left'] = True
                 player.align_change('left')
                 player.status = 'left'
+            elif event.key == pygame.K_w and not player.jumping:
+                player.jumping = 'up'
             player.anim_n = 60
         elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_w, pygame.K_a, pygame.K_d, pygame.K_s]:
-                walking = None
-                player.status = 'idle'
-                player.anim_n = 60
-    if walking == 'left':
-        all_sprites.update(camera.movement('left'))
-    elif walking == 'right':
+            if event.key == pygame.K_d:
+                walking['right'] = False
+            elif event.key == pygame.K_a:
+                walking['left'] = False
+
+    if player.jumping:
+        player.jump()
+    if walking[player.align]:
+        all_sprites.update(camera.movement(player.align))
+    elif player.align == 'left' and walking['right']:
         all_sprites.update(camera.movement('right'))
+        player.align = 'right'
+        player.status = 'right'
+        player.anim_n = 60
+    elif player.align == 'right' and walking['left']:
+        all_sprites.update(camera.movement('right'))
+        player.align = 'left'
+        player.status = 'left'
+    if not walking['left'] and not walking['right']:
+        player.status = 'idle'
+        player.anim_n = 60
     screen.fill((66, 40, 53))
     all_sprites.draw(screen)
     pygame.display.flip()
